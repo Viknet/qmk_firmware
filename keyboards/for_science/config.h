f5a7f5483cfd846b9630519adbfa872d7cae2602
Viknet <viknet@icloud.com>
2021-12-19 02:06:06 +0300

    for_science gamepad layout

--- keyboards/for_science/config.h
+++ keyboards/for_science/config.h
@@ -53,7 +53,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
-//#define MASTER_RIGHT
+#define MASTER_LEFT
 
 /*
  * Feature disable options
